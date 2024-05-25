2d8620847fd69bb40bed97db969f726b7d6febc2
ColinO'Brien <colinpobrien04@gmail.com>
2024-05-25 13:45:19 -0700

    Intervals play note on click

--- scordatura/components/string.tsx
+++ scordatura/components/string.tsx
@@ -2,20 +2,22 @@
 import React from 'react';
 
 const totalNoteMap: string[] = 
-    ['E1', 'F1', 'Fb1', 'G1', 'Gb1', 'A1', 'Ab1', 'B1', 'C2', 'Cb2', 'D2', 'Db2',
-    'E2', 'F2', 'Fb2', 'G2', 'Gb2', 'A2', 'Ab2', 'B2', 'C3', 'Cb3', 'D3', 'Db3',
-    'E3', 'F3', 'Fb3', 'G3', 'Gb3', 'A3', 'Ab3', 'B3', 'C4', 'Cb4', 'D4', 'Db4',
-    'E4', 'F4', 'Fb4', 'G4', 'Gb4', 'A4', 'Ab4', 'B4', 'C5', 'Cb5', 'D5', 'Db5',
-    'E5', 'F5', 'Fb5', 'G5', 'Gb5', 'A5', 'Ab5', 'B5', 'C6', 'Cb6', 'D6'];
+    ['E1', 'F1', 'Gb1', 'G1', 'Ab1', 'A1', 'Bb1', 'B1', 'C2', 'Db2', 'D2', 'Eb2',
+    'E2', 'F2', 'Gb2', 'G2', 'Ab2', 'A2', 'Bb2', 'B2', 'C3', 'Db3', 'D3', 'Eb3',
+    'E3', 'F3', 'Gb3', 'G3', 'Ab3', 'A3', 'Bb3', 'B3', 'C4', 'Db4', 'D4', 'Eb4',
+    'E4', 'F4', 'Gb4', 'G4', 'Ab4', 'A4', 'Bb4', 'B4', 'C5', 'Db5', 'D5', 'Eb5',
+    'E5', 'F5', 'Gb5', 'G5', 'Ab5', 'A5', 'Bb5', 'B5', 'C6', 'Db6', 'D6'];
 
 interface IntervalProps {
     string: number;
     note: string;
 }
 
-const Interval: React.FC<IntervalProps> = ({ string = 0, note = 0}) => {
+const Interval: React.FC<IntervalProps> = ({ string = 0, note}) => {
     const play = () => {
         console.log(`string ${string} note ${note}`);
+        const audio = new Audio('/sound/' + note + '.mp3');
+        audio.play();
     }
     return (
         <div className="interval" onClick={play}>
@@ -23,8 +25,6 @@ const Interval: React.FC<IntervalProps> = ({ string = 0, note = 0}) => {
         </div>
     )
 }
-
-const example: string[] = ['hi', 'hello', 'world'];
 
 interface StringProps {
     string: number;
