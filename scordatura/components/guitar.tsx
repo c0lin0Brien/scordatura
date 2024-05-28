fbed59c1acebdc2eda3c3813d14652efd17e018b
ColinO'Brien <colinpobrien04@gmail.com>
2024-05-28 11:33:01 -0700

    Tuning changes by clicking button

--- scordatura/components/guitar.tsx
+++ scordatura/components/guitar.tsx
@@ -1,18 +1,34 @@
 'use client';
-import React from 'react';
+import React, { useState } from 'react';
 import String from './string';
-
-const standardTuning: number[] = [12, 17, 22, 27, 31, 36];
+import { start } from 'repl';
 
 // TODO: Adjust level of strings available 
 const Guitar = () => {
+    const standardTuning: number[] = [12, 17, 22, 27, 31, 36];
+    const [currentTuning, setTuning] = useState([...standardTuning]);
+
+    const randomInt = (max: number) => {
+        return Math.floor(Math.random() * max);
+    }
+
+    const randomizeTuning = () => {
+        let newTuning: number[] = [];
+        for (let i = 0; i < 6; i++) {
+            let change: number = randomInt(12);
+            newTuning.push(standardTuning[i] - change);
+        }
+        console.log(newTuning);
+        setTuning(newTuning);
+    }
     // TODO: String components
 
     const strings = Array.from( {length: 6}, (_, index) => <String key={index} string={index}
-     openNote={standardTuning[index]}/>);
+     openNote={currentTuning[index]}/>);
     return (
         <div>
             {strings}
+            <button onClick={randomizeTuning}>Randomize</button>
         </div>
     )
 }
