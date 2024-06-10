d1f78d51f9b9318764b86341bc7ec039fa0e3a62
ColinO'Brien <colinpobrien04@gmail.com>
2024-06-10 11:42:24 -0700

    Scraper goes through each table and outputs chord names and notes

--- scordatura/scraper.py
+++ scordatura/scraper.py
@@ -13,12 +13,18 @@ browser = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()
 
 query = "Python"
 
-browser.get("https://www.all-guitar-chords.com/chords/index/c")
+browser.get("https://www.all-guitar-chords.com/chords/index")
 html = browser.page_source
 
-for i in range(50):
-    chordName = browser.find_element(By.XPATH, f'//main/div/div[{2 + i}]/div[1]/a')
-    chordNotes = browser.find_element(By.XPATH, f'//main/div/div[{2 + i}]/div[2]')
-    print(f"{chordName.text} => {chordNotes.text}")
+chordLinks = browser.find_elements(By.CLASS_NAME, 'pr-2')
+
+for i in chordLinks:
+    i.click()
+    time.sleep(1)
+    for j in range(50):
+        chordName = browser.find_element(By.XPATH, f'//main/div/div[{2 + j}]/div[1]/a')
+        chordNotes = browser.find_element(By.XPATH, f'//main/div/div[{2 + j}]/div[2]')
+        print(f"{chordName.text} => {chordNotes.text}")
+    browser.back()
 
 browser.quit()
